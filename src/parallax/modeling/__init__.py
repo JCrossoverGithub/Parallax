a882425ac34323bb694f04aa79269d46e1df61cb
Josh Schultz <schultzjosh0@gmail.com>
2026-08-21 11:09:16 -0400

    Add initial validation baselines

--- src/parallax/modeling/__init__.py
+++ src/parallax/modeling/__init__.py
@@ -1,5 +1,17 @@
 """Leakage-resistant modeling contracts for Parallax."""
 
+from parallax.modeling.baselines import (
+    BASELINE_EXPERIMENT_SCHEMA_VERSION,
+    CATEGORY_LABELS,
+    BaselineConfig,
+    BaselineExperimentResult,
+    BaselineModel,
+    BaselineModelingError,
+    CategoryMetrics,
+    ClassificationMetrics,
+    TrainedBaseline,
+    fit_initial_baselines,
+)
 from parallax.modeling.dataset import (
     MODELING_DATASET_SCHEMA_VERSION,
     FeaturePartition,
@@ -9,9 +21,19 @@ from parallax.modeling.dataset import (
 )
 
 __all__ = [
+    "BASELINE_EXPERIMENT_SCHEMA_VERSION",
+    "CATEGORY_LABELS",
     "MODELING_DATASET_SCHEMA_VERSION",
+    "BaselineConfig",
+    "BaselineExperimentResult",
+    "BaselineModel",
+    "BaselineModelingError",
+    "CategoryMetrics",
+    "ClassificationMetrics",
     "FeaturePartition",
     "ModelingDatasetError",
     "PartitionedFeatureDataset",
+    "TrainedBaseline",
+    "fit_initial_baselines",
     "load_partitioned_feature_dataset",
 ]
